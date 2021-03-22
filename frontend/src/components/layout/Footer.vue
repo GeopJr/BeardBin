770bfddc52fbf9899bc93ffe7433a7a6937f0467
Evangelos Paterakis <evan@geopjr.dev>
2021-03-23 00:48:20 +0200

    feat: make footer animated on small screens

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -2,8 +2,14 @@
   <v-footer class="rounded-t-xl c-footer" app absolute padless>
     <v-col class="text-center" cols="12">
       <strong class="accent--text">©</strong> {{ new Date().getFullYear() }} —
-      <strong class="accent--text">GeopJr</strong> — BeardBin is a
-      Zero-Knowledge Encrypted Text/Code sharing project written in Crystal
+      <strong class="accent--text">GeopJr</strong> —
+      <span v-if="!isMobile"
+        >BeardBin is a Zero-Knowledge Encrypted Text/Code sharing project
+        written in Crystal</span
+      >
+      <span id="loop" v-else
+        >BeardBin <span>{{ words[tmp % words.length] }}</span></span
+      >
     </v-col>
   </v-footer>
 </template>
@@ -11,6 +17,32 @@
 <script>
 export default {
   name: "Footer",
+  methods: {
+    startInterval: function () {
+      setInterval(() => {
+        this.tmp++;
+      }, 3000);
+    },
+  },
+  mounted() {
+    if (this.$vuetify.breakpoint.smAndDown) {
+      this.startInterval();
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
+  },
+  data: () => ({
+    words: [
+      "has Zero-Knowledge",
+      "is Encrypted",
+      "is a Text/Code sharing project",
+      "is written in Crystal",
+    ],
+    tmp: 0,
+  }),
 };
 </script>
 
@@ -18,5 +50,18 @@ export default {
 .c-footer {
   width: 100%;
   margin-top: 16px !important;
+}
+
+#loop > span {
+  animation: change_opacity 1.5s infinite alternate;
+}
+
+@keyframes change_opacity {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
